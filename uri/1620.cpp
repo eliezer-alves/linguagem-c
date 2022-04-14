49226f4d72e2b831a7d843a28e79eb8d359d9009
eliezer <eliezer.c.alves2015@gmail.com>
2022-04-14 08:25:24 -0300

    uri: 1620

---
+++ uri/1620.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a1 = 3, r = 2;
+	cout << size(double);
+	long double l, n, an;
+	while (scanf("%llf", &l)) {	
+		if (l==0) break;
+		n = l-2;
+		an = a1 + (n-1) * r;
+		
+		double x = double(an - l)/l;
+		
+		printf("%.6lf\n", x);
+	}
+	return 0;
+}
